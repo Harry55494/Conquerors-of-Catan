1207472c3430ec798c0255ab3d9f3377e24c6274
hp20496 <hp20496@essex.ac.uk>
2022-09-26 16:51:37 +0100

    Added roads to board

--- __main__.py
+++ __main__.py
@@ -1,17 +1,30 @@
-
 from board import *
 from player import *
 import time
 
 if __name__ == '__main__':
+
+    # Run these two lines if you haven't run the program before
+    #os.system("pip install -r requirements.txt ")
+    #os.system("clear")
+
     players = [player(1, 'blue', 'Human'), player(2, 'red', 'AI')]
     board = board(board_type='default', players=players)
 
     for building in board.buildings:
-        board.buildings[building].update({'player': players[0] if random.randint(0, 1) == 0 else players[1],
+        board.buildings[building].update({'player': players[random.randint(0, len(players)-1)],
                                           'building': 'settlement' if random.randint(0, 4) != 0 else 'city'})
         if random.randint(0,3) == 3:
             board.buildings[building].update({'player': None, 'building': None})
+
+    for road in board.roads:
+        board.roads[road].update({'player': players[random.randint(0, len(players)-1)]})
+
+        if random.randint(0,3) == 3:
+            board.roads[road].update({'player': None})
+
+    for player in players:
+        player.calculateVictoryPoints(board)
 
     player_has_won = False
 
